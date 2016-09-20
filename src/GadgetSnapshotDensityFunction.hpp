95002aaf36f7217dbcf815d70128375574166cbc
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2016-09-20 17:35:31 +0200

    Added GadgetSnapshotDensityFunction class and unit test. Still empty, as we first need HDF5 functionality.

---
+++ src/GadgetSnapshotDensityFunction.hpp
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * This file is part of CMacIonize
+ * Copyright (C) 2016 Bert Vandenbroucke (bert.vandenbroucke@gmail.com)
+ *
+ * CMacIonize is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CMacIonize is distributed in the hope that it will be useful,
+ * but WITOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CMacIonize. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+/**
+ * @file GadgetSnapshotDensityFunction.hpp
+ *
+ * @brief DensityFunction that reads a density field from a Gadget HDF5 snapshot
+ * file: header.
+ *
+ * @author Bert Vandenbroucke (bv7@st-andrews.ac.uk)
+ */
+#ifndef GADGETSNAPSHOTDENSITYFUNCTION_HPP
+#define GADGETSNAPSHOTDENSITYFUNCTION_HPP
+
+#endif // GADGETSNAPSHOTDENSITYFUNCTION_HPP
