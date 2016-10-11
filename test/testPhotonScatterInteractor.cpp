4a7a15f524217877bc7d289382dba31b58dabb40
Bert Vandenbroucke <bv7@apsient.st-andrews.ac.uk>
2016-10-11 13:01:37 +0100

    Added empty PhotonScatterInteractor skeleton and unit test.

---
+++ test/testPhotonScatterInteractor.cpp
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * This file is part of CMacIonize
+ * Copyright (C) 2016 Bert Vandenbroucke (bert.vandenbroucke@gmail.com)
+ *
+ * CMacIonize is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CMacIonize is distributed in the hope that it will be useful,
+ * but WITOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CMacIonize. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+/**
+ * @file testPhotonScatterInteractor.cpp
+ *
+ * @brief Unit test for the PhotonScatterInteractor class.
+ *
+ * @author Bert Vandenbroucke (bv7@st-andrews.ac.uk)
+ */
+
+/**
+ * @brief Unit test for the PhotonScatterInteractor class.
+ *
+ * @param argc Number of command line arguments.
+ * @param argv Command line arguments.
+ * @return Exit code: 0 on success.
+ */
+int main(int argc, char **argv) { return 0; }
