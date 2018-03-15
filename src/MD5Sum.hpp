448d71e269e2f9fa377ae73e60ed8dea885ada85
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2018-03-15 11:07:45 +0000

    Finished debugging and implementing MD5 checksum algorithm.

--- src/MD5Sum.hpp
+++ src/MD5Sum.hpp
@@ -16,10 +16,38 @@
  * along with CMacIonize. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
+/// original copyright information for the MD5 algorithm:
+
+/*
+ * Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
+ * rights reserved.
+ *
+ * License to copy and use this software is granted provided that it
+ * is identified as the "RSA Data Security, Inc. MD5 Message-Digest
+ * Algorithm" in all material mentioning or referencing this software
+ * or this function.
+ *
+ * License is also granted to make and use derivative works provided
+ * that such works are identified as "derived from the RSA Data
+ * Security, Inc. MD5 Message-Digest Algorithm" in all material
+ * mentioning or referencing the derived work.
+ *
+ * RSA Data Security, Inc. makes no representations concerning either
+ * the merchantability of this software or the suitability of this
+ * software forany particular purpose. It is provided "as is"
+ * without express or implied warranty of any kind.
+ * These notices must be retained in any copies of any part of this
+ * documentation and/or software.
+ */
+
 /**
  * @file MD5Sum.hpp
  *
  * @brief Own implementation of the MD5 checksum algorithm.
+ *
+ * This implementation is based on the Wikepedia article about MD5
+ * (https://en.wikipedia.org/wiki/MD5) and the md5sum source code, found on
+ * www.netlib.org/crc/md5sum.c.
  *
  * @author Bert Vandenbroucke (bv7@st-andrews.ac.uk)
  */
@@ -28,12 +56,9 @@
 
 #include "Error.hpp"
 
-#include <cmath>
-#include <cstring>
-#include <iostream>
+#include <fstream>
 #include <sstream>
 #include <string>
-#include <vector>
 
 /**
  * @brief Own implementation of the MD5 checksum algorithm.
@@ -50,34 +75,46 @@ inline std::string tohex(unsigned int x) {
 
   std::stringstream result;
 
-  for (unsigned int i = 0; i < 8; ++i) {
-    const unsigned char shift = (7 - i) * 4;
-    const unsigned int xshift = (x >> shift) & 15;
-    char c;
-    if (xshift < 10) {
-      c = '0' + xshift;
-    } else {
-      c = 'a' + (xshift - 10);
+  // the weird code is a consequence of the weird big endian/little endian mix
+  // up in the original algorithm
+  for (unsigned int i = 0; i < 4; ++i) {
+    for (unsigned int j = 0; j < 2; ++j) {
+      const unsigned char shift = (2 * i + 1 - j) * 4;
+      const unsigned int xshift = (x >> shift) & 15;
+      char c;
+      if (xshift < 10) {
+        c = '0' + xshift;
+      } else {
+        c = 'a' + (xshift - 10);
+      }
+      result << c;
     }
-    result << c;
   }
 
   return result.str();
 }
 
 /**
- * @brief Get the MD5 checksum for the file with the given name.
+ * @brief Perform the MD5 algorithm on a 512 bit block of data.
  *
- * @param filename Name of an existing file.
- * @return MD5 checksum of that file.
+ * @param message 512 bit block to process.
+ * @param a0 Current state of the A block (is updated).
+ * @param b0 Current state of the B block (is updated).
+ * @param c0 Current state of the C block (is updated).
+ * @param d0 Current state of the D block (is updated).
  */
-inline std::string get_checksum(std::string filename) {
+inline void md5(unsigned char message[64], unsigned int &a0, unsigned int &b0,
+                unsigned int &c0, unsigned int &d0) {
 
+  // per round rotations
   const unsigned int s[64] = {
       7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
       5, 9,  14, 20, 5, 9,  14, 20, 5, 9,  14, 20, 5, 9,  14, 20,
       4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
       6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
+
+  // per round extra term, equal to the integer part of 2^32 x abs(sin(i)),
+  // i = 1-64
   const unsigned int K[64] = {
       0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a,
       0xa8304613, 0xfd469501, 0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
@@ -91,107 +128,167 @@ inline std::string get_checksum(std::string filename) {
       0xffeff47d, 0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
       0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
 
+  unsigned int M[16];
+  for (unsigned int j = 0; j < 16; ++j) {
+    M[j] = message[4 * j + 3];
+    M[j] <<= 8;
+    M[j] |= message[4 * j + 2];
+    M[j] <<= 8;
+    M[j] |= message[4 * j + 1];
+    M[j] <<= 8;
+    M[j] |= message[4 * j + 0];
+  }
+
+  unsigned int A = a0;
+  unsigned int B = b0;
+  unsigned int C = c0;
+  unsigned int D = d0;
+
+  for (unsigned int i = 0; i < 64; ++i) {
+    unsigned int F, g;
+    switch (i >> 4) {
+    case 0:
+      F = (B & C) | ((~B) & D);
+      g = i;
+      break;
+    case 1:
+      F = (D & B) | ((~D) & C);
+      g = (5 * i + 1) % 16;
+      break;
+    case 2:
+      F = B ^ C ^ D;
+      g = (3 * i + 5) % 16;
+      break;
+    case 3:
+      F = C ^ (B | (~D));
+      g = (7 * i) % 16;
+      break;
+    }
+    F = F + A + K[i] + M[g];
+    A = D;
+    D = C;
+    C = B;
+    B = B + ((F << s[i]) | (F >> (32 - s[i])));
+  }
+  a0 += A;
+  b0 += B;
+  c0 += C;
+  d0 += D;
+}
+
+/**
+ * @brief Get the MD5 checksum for the given input stream.
+ *
+ * @param input Input stream.
+ * @return MD5 checksum of the stream.
+ */
+inline std::string get_checksum(std::istream &input) {
+
+  // initial hash values
   unsigned int a0 = 0x67452301;
   unsigned int b0 = 0xefcdab89;
   unsigned int c0 = 0x98badcfe;
   unsigned int d0 = 0x10325476;
 
-  // for now, we assume the filename is the actual message
-  std::vector< unsigned char > message(filename.size(), 0);
-  for (unsigned int i = 0; i < filename.size(); ++i) {
-    message[i] = filename[i];
-  }
-
-  // add 1 bit to end of message
-  message.push_back(128);
-
-  // append 0 bits until the length of the message in bits%512 == 448
-  // (or 56 bytes)
-  while (message.size() % 64 != 56) {
-    message.push_back(0);
-  }
-
-  // add original length of message in bits as 64 bits to end
-  unsigned long size = filename.size() << 3;
-  for (unsigned int i = 0; i < 8; ++i) {
-    const unsigned char shift = (7 - i) * 8;
-    const unsigned int sizeshift = (size >> shift) & 255;
-    message.push_back(sizeshift);
-  }
-
-  //  cmac_status("message size: %lu", message.size());
-  //  cmac_status("message:");
-  //  for (unsigned int i = 0; i < message.size(); ++i) {
-  //    const unsigned char high = (message[i] >> 4) & 15;
-  //    const unsigned char low = message[i] & 15;
-  //    char chigh, clow;
-  //    if (high < 10) {
-  //      chigh = '0' + high;
-  //    } else {
-  //      chigh = 'a' + (high - 10);
-  //    }
-  //    if (low < 10) {
-  //      clow = '0' + low;
-  //    } else {
-  //      clow = 'a' + (low - 10);
-  //    }
-  //    std::cout << chigh << clow;
-  //  }
-  //  std::cout << std::endl;
-
-  // now process the message
-
-  for (unsigned int ichunk = 0; ichunk < message.size(); ichunk += 64) {
-    unsigned int M[16];
-    for (unsigned int j = 0; j < 16; ++j) {
-      M[j] = message[ichunk * 64 + 4 * j + 3];
-      M[j] <<= 8;
-      M[j] |= message[ichunk * 64 + 4 * j + 2];
-      M[j] <<= 8;
-      M[j] |= message[ichunk * 64 + 4 * j + 1];
-      M[j] <<= 8;
-      M[j] |= message[ichunk * 64 + 4 * j + 0];
-      //      cmac_status("M[%u] = %s", j, tohex(M[j]).c_str());
+  char c;
+  unsigned char message[64];
+  unsigned long message_size = 0;
+  bool special_end = true;
+  while (input.get(c)) {
+    message[0] = c;
+    ++message_size;
+    unsigned char current_index = 1;
+    while (current_index < 64 && input.get(c)) {
+      message[current_index] = c;
+      ++message_size;
+      ++current_index;
     }
-
-    unsigned int A = a0;
-    unsigned int B = b0;
-    unsigned int C = c0;
-    unsigned int D = d0;
-
-    for (unsigned int i = 0; i < 64; ++i) {
-      unsigned int F, g;
-      switch (i >> 4) {
-      case 0:
-        F = (B & C) | ((~B) & D);
-        g = i;
-        break;
-      case 1:
-        F = (D & B) | ((~D) & C);
-        g = (5 * i + 1) % 16;
-        break;
-      case 2:
-        F = B ^ C ^ D;
-        g = (3 * i + 5) % 16;
-        break;
-      case 3:
-        F = C ^ (B | (~D));
-        g = (7 * i) % 16;
-        break;
+    if (current_index < 64) {
+      // we reached the end of the stream
+      // we need to add an extra 1 bit and 8 extra bytes of length data
+      // add the extra 1 bit
+      message[current_index] = 128;
+      ++current_index;
+      // make sure we don't add the extra 1 bit again
+      special_end = false;
+      if (current_index < 57) {
+        // pad and add the length data
+        while (current_index < 56) {
+          message[current_index] = 0;
+          ++current_index;
+        }
+        message_size <<= 3;
+        for (unsigned int i = 0; i < 8; ++i) {
+          const unsigned char shift = i * 8;
+          const unsigned int sizeshift = (message_size >> shift) & 255;
+          message[current_index] = sizeshift;
+          ++current_index;
+        }
+        // make sure we do not add the length data again
+        message_size = 0;
+      } else {
+        // no space for length data: pad with zeroes
+        // length data will be added in a new 512 bit block
+        while (current_index < 64) {
+          message[current_index] = 0;
+          ++current_index;
+        }
       }
-      F = F + A + K[i] + M[g];
-      A = D;
-      D = C;
-      C = B;
-      B = B + ((F << s[i]) | (F >> (32 - s[i])));
     }
-    a0 += A;
-    b0 += B;
-    c0 += C;
-    d0 += D;
+
+    // process the current message
+    md5(message, a0, b0, c0, d0);
   }
 
+  // add an extra 512 bit block if necessary
+  if (special_end || message_size > 0) {
+    unsigned int current_index = 0;
+    if (special_end) {
+      // add the extra 1 bit
+      message[current_index] = 128;
+      ++current_index;
+    }
+    // pad and add the length data
+    while (current_index < 56) {
+      message[current_index] = 0;
+      ++current_index;
+    }
+    message_size <<= 3;
+    for (unsigned int i = 0; i < 8; ++i) {
+      const unsigned char shift = i * 8;
+      const unsigned int sizeshift = (message_size >> shift) & 255;
+      message[current_index] = sizeshift;
+      ++current_index;
+    }
+
+    // now process the final block
+    md5(message, a0, b0, c0, d0);
+  }
+
+  // return the hexadecimal checksum
   return tohex(a0) + tohex(b0) + tohex(c0) + tohex(d0);
+}
+
+/**
+ * @brief Get the MD5 checksum for the given input string.
+ *
+ * @param message Input string.
+ * @return MD5 checksum of the string.
+ */
+inline std::string get_checksum(std::string message) {
+  std::istringstream stringstream(message);
+  return get_checksum(stringstream);
+}
+
+/**
+ * @brief Get the MD5 checksum for the file with the given name.
+ *
+ * @param filename Name of a file.
+ * @return MD5 checksum for that file.
+ */
+inline std::string get_file_checksum(std::string filename) {
+  std::ifstream filestream(filename);
+  return get_checksum(filestream);
 }
 }
 
