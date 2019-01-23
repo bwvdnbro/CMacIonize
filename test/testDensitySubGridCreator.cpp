23bbfe8cd3ffd6a2337350d44d9a12c37379f739
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2019-01-23 17:17:39 +0000

    Extended unit test for DensitySubGridCreator::create_copies.

--- test/testDensitySubGridCreator.cpp
+++ test/testDensitySubGridCreator.cpp
@@ -164,11 +164,347 @@ int main(int argc, char **argv) {
 
   std::vector< uint_fast8_t > copy_levels(ngrid, 0);
   copy_levels[82] = 1;
+  copy_levels[83] = 2;
   std::vector< uint_fast32_t > originals, copies;
   grid_creator.create_copies(subgrids, copy_levels, originals, copies);
 
+  assert_condition(subgrids.size() == 132);
+  assert_condition(originals.size() == 4);
+  assert_condition(copies.size() == ngrid);
+
   assert_condition(originals[0] == 82);
   assert_condition(copies[82] == ngrid);
+  assert_condition(originals[1] == 83);
+  assert_condition(copies[83] == ngrid + 1);
+  assert_condition(originals[2] == 83);
+  assert_condition(originals[3] == 83);
+
+  // we repeat the neighbour checks for subgrid 82
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_CORNER_NNN) ==
+                   41);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_CORNER_NNP) ==
+                   43);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_CORNER_NPN) ==
+                   57);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_CORNER_NPP) ==
+                   59);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_CORNER_PNN) ==
+                   105);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_CORNER_PNP) ==
+                   107);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_CORNER_PPN) ==
+                   121);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_CORNER_PPP) ==
+                   123);
+
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_EDGE_X_NN) ==
+                   73);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_EDGE_X_NP) ==
+                   75);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_EDGE_X_PN) ==
+                   89);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_EDGE_X_PP) ==
+                   91);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_EDGE_Y_NN) ==
+                   49);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_EDGE_Y_NP) ==
+                   51);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_EDGE_Y_PN) ==
+                   113);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_EDGE_Y_PP) ==
+                   115);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_EDGE_Z_NN) ==
+                   42);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_EDGE_Z_NP) ==
+                   58);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_EDGE_Z_PN) ==
+                   106);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_EDGE_Z_PP) ==
+                   122);
+
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_FACE_X_N) == 50);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_FACE_X_P) ==
+                   114);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_FACE_Y_N) == 74);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_FACE_Y_P) == 90);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_FACE_Z_N) == 81);
+  assert_condition(subgrids[82]->get_neighbour(TRAVELDIRECTION_FACE_Z_P) == 83);
+
+  // also check the copy of 82
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_CORNER_NNN) ==
+                   41);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_CORNER_NNP) ==
+                   43);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_CORNER_NPN) ==
+                   57);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_CORNER_NPP) ==
+                   59);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_CORNER_PNN) ==
+                   105);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_CORNER_PNP) ==
+                   107);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_CORNER_PPN) ==
+                   121);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_CORNER_PPP) ==
+                   123);
+
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_EDGE_X_NN) ==
+                   73);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_EDGE_X_NP) ==
+                   75);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_EDGE_X_PN) ==
+                   89);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_EDGE_X_PP) ==
+                   91);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_EDGE_Y_NN) ==
+                   49);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_EDGE_Y_NP) ==
+                   51);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_EDGE_Y_PN) ==
+                   113);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_EDGE_Y_PP) ==
+                   115);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_EDGE_Z_NN) ==
+                   42);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_EDGE_Z_NP) ==
+                   58);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_EDGE_Z_PN) ==
+                   106);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_EDGE_Z_PP) ==
+                   122);
+
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_FACE_X_N) ==
+                   50);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_FACE_X_P) ==
+                   114);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_FACE_Y_N) ==
+                   74);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_FACE_Y_P) ==
+                   90);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_FACE_Z_N) ==
+                   81);
+  assert_condition(subgrids[128]->get_neighbour(TRAVELDIRECTION_FACE_Z_P) ==
+                   129);
+
+  // check subgrid 83 (2, 2, 3)
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_CORNER_NNN) ==
+                   42);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_CORNER_NNP) ==
+                   44);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_CORNER_NPN) ==
+                   58);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_CORNER_NPP) ==
+                   60);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_CORNER_PNN) ==
+                   106);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_CORNER_PNP) ==
+                   108);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_CORNER_PPN) ==
+                   122);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_CORNER_PPP) ==
+                   124);
+
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_EDGE_X_NN) ==
+                   74);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_EDGE_X_NP) ==
+                   76);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_EDGE_X_PN) ==
+                   90);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_EDGE_X_PP) ==
+                   92);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_EDGE_Y_NN) ==
+                   50);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_EDGE_Y_NP) ==
+                   52);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_EDGE_Y_PN) ==
+                   114);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_EDGE_Y_PP) ==
+                   116);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_EDGE_Z_NN) ==
+                   43);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_EDGE_Z_NP) ==
+                   59);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_EDGE_Z_PN) ==
+                   107);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_EDGE_Z_PP) ==
+                   123);
+
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_FACE_X_N) == 51);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_FACE_X_P) ==
+                   115);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_FACE_Y_N) == 75);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_FACE_Y_P) == 91);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_FACE_Z_N) == 82);
+  assert_condition(subgrids[83]->get_neighbour(TRAVELDIRECTION_FACE_Z_P) == 84);
+
+  // check the 3 copies of subgrid 83:
+  //  - copy 1
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_CORNER_NNN) ==
+                   42);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_CORNER_NNP) ==
+                   44);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_CORNER_NPN) ==
+                   58);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_CORNER_NPP) ==
+                   60);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_CORNER_PNN) ==
+                   106);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_CORNER_PNP) ==
+                   108);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_CORNER_PPN) ==
+                   122);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_CORNER_PPP) ==
+                   124);
+
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_EDGE_X_NN) ==
+                   74);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_EDGE_X_NP) ==
+                   76);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_EDGE_X_PN) ==
+                   90);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_EDGE_X_PP) ==
+                   92);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_EDGE_Y_NN) ==
+                   50);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_EDGE_Y_NP) ==
+                   52);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_EDGE_Y_PN) ==
+                   114);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_EDGE_Y_PP) ==
+                   116);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_EDGE_Z_NN) ==
+                   43);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_EDGE_Z_NP) ==
+                   59);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_EDGE_Z_PN) ==
+                   107);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_EDGE_Z_PP) ==
+                   123);
+
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_FACE_X_N) ==
+                   51);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_FACE_X_P) ==
+                   115);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_FACE_Y_N) ==
+                   75);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_FACE_Y_P) ==
+                   91);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_FACE_Z_N) ==
+                   82);
+  assert_condition(subgrids[129]->get_neighbour(TRAVELDIRECTION_FACE_Z_P) ==
+                   84);
+
+  //  - copy 2
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_CORNER_NNN) ==
+                   42);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_CORNER_NNP) ==
+                   44);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_CORNER_NPN) ==
+                   58);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_CORNER_NPP) ==
+                   60);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_CORNER_PNN) ==
+                   106);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_CORNER_PNP) ==
+                   108);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_CORNER_PPN) ==
+                   122);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_CORNER_PPP) ==
+                   124);
+
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_EDGE_X_NN) ==
+                   74);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_EDGE_X_NP) ==
+                   76);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_EDGE_X_PN) ==
+                   90);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_EDGE_X_PP) ==
+                   92);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_EDGE_Y_NN) ==
+                   50);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_EDGE_Y_NP) ==
+                   52);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_EDGE_Y_PN) ==
+                   114);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_EDGE_Y_PP) ==
+                   116);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_EDGE_Z_NN) ==
+                   43);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_EDGE_Z_NP) ==
+                   59);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_EDGE_Z_PN) ==
+                   107);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_EDGE_Z_PP) ==
+                   123);
+
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_FACE_X_N) ==
+                   51);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_FACE_X_P) ==
+                   115);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_FACE_Y_N) ==
+                   75);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_FACE_Y_P) ==
+                   91);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_FACE_Z_N) ==
+                   128);
+  assert_condition(subgrids[130]->get_neighbour(TRAVELDIRECTION_FACE_Z_P) ==
+                   84);
+
+  //  - copy 3
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_CORNER_NNN) ==
+                   42);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_CORNER_NNP) ==
+                   44);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_CORNER_NPN) ==
+                   58);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_CORNER_NPP) ==
+                   60);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_CORNER_PNN) ==
+                   106);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_CORNER_PNP) ==
+                   108);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_CORNER_PPN) ==
+                   122);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_CORNER_PPP) ==
+                   124);
+
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_EDGE_X_NN) ==
+                   74);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_EDGE_X_NP) ==
+                   76);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_EDGE_X_PN) ==
+                   90);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_EDGE_X_PP) ==
+                   92);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_EDGE_Y_NN) ==
+                   50);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_EDGE_Y_NP) ==
+                   52);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_EDGE_Y_PN) ==
+                   114);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_EDGE_Y_PP) ==
+                   116);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_EDGE_Z_NN) ==
+                   43);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_EDGE_Z_NP) ==
+                   59);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_EDGE_Z_PN) ==
+                   107);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_EDGE_Z_PP) ==
+                   123);
+
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_FACE_X_N) ==
+                   51);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_FACE_X_P) ==
+                   115);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_FACE_Y_N) ==
+                   75);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_FACE_Y_P) ==
+                   91);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_FACE_Z_N) ==
+                   128);
+  assert_condition(subgrids[131]->get_neighbour(TRAVELDIRECTION_FACE_Z_P) ==
+                   84);
 
   for (size_t igrid = 0; igrid < subgrids.size(); ++igrid) {
     delete subgrids[igrid];
