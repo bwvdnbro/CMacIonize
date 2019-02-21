85bf5ec5870531d5ee3464fc2d1ccdda63aa0ec2
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2019-02-21 14:02:10 +0000

    Fixed a bug in IsotropicContinuousPhotonSource.

--- src/IsotropicContinuousPhotonSource.hpp
+++ src/IsotropicContinuousPhotonSource.hpp
@@ -43,7 +43,7 @@
 class IsotropicContinuousPhotonSource : public ContinuousPhotonSource {
 private:
   /*! @brief Box in which the radiation enters. */
-  const Box<> &_box;
+  const Box<> _box;
 
 public:
   /**
@@ -52,7 +52,7 @@ public:
    * @param simulation_box Simulation box (in m).
    * @param log Log to write logging info to.
    */
-  IsotropicContinuousPhotonSource(const Box<> &simulation_box,
+  IsotropicContinuousPhotonSource(const Box<> simulation_box,
                                   Log *log = nullptr)
       : _box(simulation_box) {
 
