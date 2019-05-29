b32d37607a68a3cd2aa3d7c53394a54c8a8d40e2
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2019-05-29 12:28:33 +0100

    Added VelocityPDFCalculator and unit test.

--- src/LiveOutputManager.hpp
+++ src/LiveOutputManager.hpp
@@ -30,6 +30,7 @@
 #include "ParameterFile.hpp"
 #include "SurfaceDensityCalculator.hpp"
 #include "Utilities.hpp"
+#include "VelocityPDFCalculator.hpp"
 
 /**
  * @brief Class that manages live output classes.
@@ -51,6 +52,9 @@ private:
   /*! @brief DensityPDFCalculator (if live output enabled). */
   DensityPDFCalculator *_density_PDF_calculator;
 
+  /*! @brief VelocityPDFCalculator (if live output enabled). */
+  VelocityPDFCalculator *_velocity_PDF_calculator;
+
 public:
   /**
    * @brief Constructor.
@@ -64,6 +68,9 @@ public:
    * @param minimum_density Minimum density for the density PDF (in kg m^-3).
    * @param maximum_density Maximum density for the density PDF (in kg m^-3).
    * @param number_of_density_bins Number of bins in the density PDF.
+   * @param output_velocity_PDF Output the velocity PDF?
+   * @param maximum_velocity Maximum velocity for the velocity PDF (in m s^-1).
+   * @param number_of_velocity_bins Number of bins in the velocity PDF.
    * @param output_interval Output interval (in s).
    */
   inline LiveOutputManager(
@@ -72,9 +79,11 @@ public:
       const bool enabled, const bool output_surface_density,
       const bool output_density_PDF, const double minimum_density,
       const double maximum_density, const uint_fast32_t number_of_density_bins,
-      const double output_interval)
+      const bool output_velocity_PDF, const double maximum_velocity,
+      const uint_fast32_t number_of_velocity_bins, const double output_interval)
       : _enabled(enabled), _output_interval(output_interval), _next_output(0),
-        _surface_density_calculator(nullptr), _density_PDF_calculator(nullptr) {
+        _surface_density_calculator(nullptr), _density_PDF_calculator(nullptr),
+        _velocity_PDF_calculator(nullptr) {
 
     if (_enabled) {
       if (output_surface_density) {
@@ -87,6 +96,13 @@ public:
             number_of_subgrids.x() * number_of_subgrids.y() *
                 number_of_subgrids.z(),
             minimum_density, maximum_density, number_of_density_bins);
+      }
+
+      if (output_velocity_PDF) {
+        _velocity_PDF_calculator = new VelocityPDFCalculator(
+            number_of_subgrids.x() * number_of_subgrids.y() *
+                number_of_subgrids.z(),
+            maximum_velocity, number_of_velocity_bins);
       }
     }
   }
@@ -104,6 +120,11 @@ public:
    *  - maximum density: Upper limit for the density PDF (default:
    *    1.e-19 g cm^-3)
    *  - number of density bins: Number of bins in the density PDF (default: 100)
+   *  - output velocity PDF: Output velocity PDF? (default: true)
+   *  - maximum velocity: Upper limit for the velocity PDF (default:
+   *    50. km s^-1)
+   *  - number of velocity bins: Number of bins in the velocity PDF (default:
+   *    100)
    *  - output interval: Interval between consecutive outputs (default: 1. s)
    *
    * @param number_of_subgrids Number of subgrids in each coordinate direction.
@@ -128,6 +149,12 @@ public:
                 "LiveOutputManager:maximum density", "1.e-19 g cm^-3"),
             params.get_value< uint_fast32_t >(
                 "LiveOutputManager:number of density bins", 100),
+            params.get_value< bool >("LiveOutputManager:output velocity PDF",
+                                     true),
+            params.get_physical_value< QUANTITY_VELOCITY >(
+                "LiveOutputManager:maximum velocity", "50. km s^-1"),
+            params.get_value< uint_fast32_t >(
+                "LiveOutputManager:number of velocity bins", 100),
             params.get_physical_value< QUANTITY_TIME >(
                 "LiveOutputManager:output interval", "1. s")) {}
 
@@ -140,6 +167,9 @@ public:
     }
     if (_density_PDF_calculator) {
       delete _density_PDF_calculator;
+    }
+    if (_velocity_PDF_calculator) {
+      delete _velocity_PDF_calculator;
     }
   }
 
@@ -171,6 +201,10 @@ public:
     if (_density_PDF_calculator) {
       _density_PDF_calculator->calculate_density_PDF(index, subgrid);
     }
+
+    if (_velocity_PDF_calculator) {
+      _velocity_PDF_calculator->calculate_velocity_PDF(index, subgrid);
+    }
   }
 
   /**
@@ -190,6 +224,12 @@ public:
       std::string filename = Utilities::compose_filename(
           ".", "density_PDF_", "txt", _next_output, 4);
       _density_PDF_calculator->output(filename);
+    }
+
+    if (_velocity_PDF_calculator) {
+      std::string filename = Utilities::compose_filename(
+          ".", "velocity_PDF_", "txt", _next_output, 4);
+      _velocity_PDF_calculator->output(filename);
     }
 
     ++_next_output;
