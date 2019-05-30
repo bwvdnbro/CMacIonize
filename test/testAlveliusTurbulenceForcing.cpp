1e1814289e01151c6030abab4f9085494f00a419
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2019-05-30 13:06:42 +0100

    Alvelius forcing now uses predcomputed sine and cosine tables.

--- test/testAlveliusTurbulenceForcing.cpp
+++ test/testAlveliusTurbulenceForcing.cpp
@@ -36,28 +36,26 @@
  */
 int main(int argc, char **argv) {
 
-  AlveliusTurbulenceForcing forcing(1., 2., 3., 2.5, 0.2, 1., 42, 1.e-6, 0.);
+  AlveliusTurbulenceForcing forcing(1, 32, Box<>(0., 1.), 2., 3., 2.5, 0.2, 1.,
+                                    42, 1.e-6, 0.);
 
   double box[6] = {0., 0., 0., 1., 1., 1.};
   CoordinateVector< int_fast32_t > ncell(32, 32, 32);
-  HydroDensitySubGrid subgrid(box, ncell);
 
+  HydroDensitySubGrid subgrid(box, ncell);
   for (auto cellit = subgrid.hydro_begin(); cellit != subgrid.hydro_end();
        ++cellit) {
     cellit.get_hydro_variables().conserved(0) = 1.;
   }
 
-  forcing.update_turbulence(1.e-5);
-  forcing.add_turbulent_forcing(subgrid);
+  HydroDensitySubGrid subgrid2(box, ncell);
+  for (auto cellit = subgrid2.hydro_begin(); cellit != subgrid2.hydro_end();
+       ++cellit) {
+    cellit.get_hydro_variables().conserved(0) = 1.;
+  }
 
-  {
-    RestartWriter writer("test_alvelius.restart");
-    forcing.write_restart_file(writer);
-  }
-  {
-    RestartReader reader("test_alvelius.restart");
-    AlveliusTurbulenceForcing forcing2(reader);
-  }
+  forcing.update_turbulence(1.e-5);
+  forcing.add_turbulent_forcing(0, subgrid);
 
   std::ofstream ofile("test_alvelius.txt");
   ofile << "#x (m)\ty (m)\tz (m)\tvx (m s^-1)\tvy (m s^-1)\tvz (m s^-1)\n";
@@ -69,6 +67,44 @@ int main(int argc, char **argv) {
         cellit.get_hydro_variables().get_conserved_momentum() / m;
     ofile << x.x() << "\t" << x.y() << "\t" << x.z() << "\t" << v.x() << "\t"
           << v.y() << "\t" << v.z() << "\n";
+  }
+
+  forcing.add_turbulent_forcing(0, subgrid2);
+
+  {
+    RestartWriter writer("test_alvelius.restart");
+    forcing.write_restart_file(writer);
+  }
+
+  forcing.update_turbulence(1.e-5);
+  forcing.add_turbulent_forcing(0, subgrid);
+
+  {
+    RestartReader reader("test_alvelius.restart");
+    AlveliusTurbulenceForcing forcing2(reader);
+    forcing2.update_turbulence(1.e-5);
+
+    auto cellit1 = subgrid.hydro_begin();
+    auto cellit2 = subgrid2.hydro_begin();
+    while (cellit1 != subgrid.hydro_end()) {
+
+      const CoordinateVector<> p1 = cellit1.get_cell_midpoint();
+      const CoordinateVector<> p2 = cellit2.get_cell_midpoint();
+      assert_condition(p1.x() == p2.x());
+      assert_condition(p1.y() == p2.y());
+      assert_condition(p1.z() == p2.z());
+
+      const CoordinateVector<> v1 =
+          cellit1.get_hydro_variables().get_primitives_velocity();
+      const CoordinateVector<> v2 =
+          cellit2.get_hydro_variables().get_primitives_velocity();
+      assert_condition(v1.x() == v2.x());
+      assert_condition(v1.y() == v2.y());
+      assert_condition(v1.z() == v2.z());
+
+      ++cellit1;
+      ++cellit2;
+    }
   }
 
   return 0;
