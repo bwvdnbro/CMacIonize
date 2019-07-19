0b32e588931cf37653235425c19d647417719f0a
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2019-07-19 17:26:24 +0100

    Time log web page now contains subpages for all blocks.

--- tools/generate_timeline_overview.py
+++ tools/generate_timeline_overview.py
@@ -41,86 +41,121 @@ import shutil
 
 chart_index = 0
 
+
 class TimelineChart:
-  def __init__(self, starts, ends, labels, levels):
-    global chart_index
-    self._starts = starts
-    self._ends = ends
-    self._labels = labels
-    self._levels = levels
-    self._index = chart_index
-    chart_index += 1
+    def __init__(self, starts, ends, labels, levels):
+        global chart_index
+        self._starts = starts
+        self._ends = ends
+        self._labels = labels
+        self._levels = levels
+        self._index = chart_index
+        chart_index += 1
 
-  def write_callback(self, ofile):
-    ofile.write("google.charts.setOnLoadCallback(drawChart{0});".format(
-      self._index))
+    def write_callback(self, ofile):
+        ofile.write(
+            "google.charts.setOnLoadCallback(drawChart{0});".format(self._index)
+        )
 
-  def write_function(self, ofile):
-    ofile.write("function drawChart{0}() {{".format(self._index))
-    ofile.write("var data = new google.visualization.DataTable();")
-    ofile.write("data.addColumn({type: 'string', id: 'Level'});")
-    ofile.write("data.addColumn({type: 'string', id: 'Label'});")
-    ofile.write("data.addColumn({type: 'number', id: 'Start'});")
-    ofile.write("data.addColumn({type: 'number', id: 'End'});")
-    ofile.write("data.addRows([")
-    for i in range(len(self._labels)):
-      ofile.write("['Level {0}', '{1}', {2}, {3}],".format(
-        self._levels[i], self._labels[i], 1000. * self._starts[i],
-        1000. * self._ends[i]))
-    ofile.write("]);")
-    ofile.write("var chart = new google.visualization.Timeline("
-                " document.getElementById('timelinechart{0}'));".format(
-      self._index))
-    ofile.write("chart.draw(data)}")
+    def write_function(self, ofile):
+        ofile.write("function drawChart{0}() {{".format(self._index))
+        ofile.write("var data = new google.visualization.DataTable();")
+        ofile.write("data.addColumn({type: 'string', id: 'Level'});")
+        ofile.write("data.addColumn({type: 'string', id: 'Label'});")
+        ofile.write("data.addColumn({type: 'number', id: 'Start'});")
+        ofile.write("data.addColumn({type: 'number', id: 'End'});")
+        ofile.write("data.addRows([")
+        for i in range(len(self._labels)):
+            ofile.write(
+                "['Level {0}', '{1}', {2}, {3}],".format(
+                    self._levels[i],
+                    self._labels[i],
+                    1000.0 * self._starts[i],
+                    1000.0 * self._ends[i],
+                )
+            )
+        ofile.write("]);")
+        ofile.write(
+            "var chart = new google.visualization.Timeline("
+            " document.getElementById('timelinechart{0}'));".format(self._index)
+        )
+        ofile.write("chart.draw(data)}")
 
-  def write_div(self, ofile):
-    ofile.write("<div id=\"timelinechart{0}\"".format(self._index))
-    ofile.write(" style=\"width: 100%; height: 250px;\"></div>")
+    def write_div(self, ofile):
+        ofile.write('<div id="timelinechart{0}"'.format(self._index))
+        ofile.write(' style="width: 100%; height: 250px;"></div>')
+
 
 class PieChart:
-  def __init__(self, wedges, labels, level):
-    global chart_index
-    self._wedges = wedges
-    self._labels = labels
-    self._index = chart_index
-    self._level = level
-    chart_index += 1
+    def __init__(self, wedges, labels, level, ids):
+        global chart_index
+        self._wedges = wedges
+        self._labels = labels
+        self._index = chart_index
+        self._level = level
+        self._ids = ids
+        chart_index += 1
 
-  def write_callback(self, ofile):
-    ofile.write("google.charts.setOnLoadCallback(drawChart{0});".format(
-      self._index))
+    def write_callback(self, ofile):
+        ofile.write(
+            "google.charts.setOnLoadCallback(drawChart{0});".format(self._index)
+        )
 
-  def write_function(self, ofile):
-    ofile.write("function drawChart{0}() {{".format(self._index))
-    ofile.write("var data = google.visualization.arrayToDataTable([")
-    ofile.write("['Task', 'Total time (s)'],")
-    for i in range(len(self._wedges)):
-      ofile.write("['{0}', {1}],".format(self._labels[i], self._wedges[i]))
-    ofile.write("]);")
-    ofile.write("var options = {{title: 'Level {0}'}};".format(self._level))
-    ofile.write("var chart = new google.visualization.PieChart("
-                " document.getElementById('piechart{0}'));".format(self._index))
-    ofile.write("chart.draw(data, options)}")
+    def write_function(self, ofile):
+        ofile.write("function drawChart{0}() {{".format(self._index))
+        ofile.write("var data = google.visualization.arrayToDataTable([")
+        ofile.write("['Task', 'Total time (s)', 'ID'],")
+        for i in range(len(self._wedges)):
+            ofile.write(
+                "['{0}', {1}, {2}],".format(
+                    self._labels[i], self._wedges[i], self._ids[i]
+                )
+            )
+        ofile.write("]);")
+        ofile.write("var options = {{title: 'Level {0}'}};".format(self._level))
+        ofile.write(
+            "var chart = new google.visualization.PieChart("
+            " document.getElementById('piechart{0}'));".format(self._index)
+        )
+        ofile.write(
+            """  // The select handler. Call the chart's getSelection() method
+  function selectHandler() {
+    var selectedItem = chart.getSelection()[0];
+    if (selectedItem) {
+      var value = data.getValue(selectedItem.row, 2);
+      window.location = "block" + value + ".html";
+    }
+  }
 
-  def write_div(self, ofile):
-    ofile.write("<div id=\"piechart{0}\"".format(self._index))
-    ofile.write(" style=\"width: 100%; height: 500px;\"></div>")
+  // Listen for the 'select' event, and call my function selectHandler() when
+  // the user selects something on the chart.
+  google.visualization.events.addListener(chart, 'select', selectHandler);"""
+        )
+        ofile.write("chart.draw(data, options)}")
+
+    def write_div(self, ofile):
+        ofile.write('<div id="piechart{0}"'.format(self._index))
+        ofile.write(' style="width: 100%; height: 500px;"></div>')
+
 
 class HTMLFile:
-  def __init__(self):
-    self._charts = []
-    self._widths = []
+    def __init__(self, title, return_link=False):
+        self._charts = []
+        self._widths = []
+        self._title = title
+        self._return_link = return_link
 
-  def add_chart(self, chart, full_width = False):
-    self._charts.append(chart)
-    if full_width:
-      self._widths.append(12)
-    else:
-      self._widths.append(6)
+    def add_chart(self, chart, full_width=False):
+        self._charts.append(chart)
+        if full_width:
+            self._widths.append(12)
+        else:
+            self._widths.append(6)
 
-  def write(self, fname):
-    ofile = open(fname, "w")
-    ofile.write("""<html><head><script type="text/javascript"
+    def write(self, fname):
+        ofile = open(fname, "w")
+        ofile.write(
+            """<html><head><script type="text/javascript"
 src="https://www.gstatic.com/charts/loader.js"></script>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -129,22 +164,27 @@ src="https://www.gstatic.com/charts/loader.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <script type="text/javascript">
-google.charts.load('current', {'packages':['corechart', 'timeline']});""")
+google.charts.load('current', {'packages':['corechart', 'timeline']});"""
+        )
 
-    for chart in self._charts:
-      chart.write_callback(ofile)
-    for chart in self._charts:
-      chart.write_function(ofile)
-    ofile.write("</script></head><body>")
-    ofile.write("<div class=\"jumbotron text-center\">")
-    ofile.write("<h1>Simulation time line</h1></div>")
-    ofile.write("<div class=\"container-fluid\"><div class=\"row\">")
-    for i in range(len(self._charts)):
-      ofile.write("<div class=\"col-sm-{0}\">".format(self._widths[i]))
-      self._charts[i].write_div(ofile)
-      ofile.write("</div>")
-    ofile.write("</div></div>")
-    ofile.write("</body></html>")
+        for chart in self._charts:
+            chart.write_callback(ofile)
+        for chart in self._charts:
+            chart.write_function(ofile)
+        ofile.write("</script></head><body>")
+        ofile.write('<div class="jumbotron text-center">')
+        ofile.write("<h1>{0}</h1>".format(self._title))
+        if self._return_link:
+            ofile.write('<p><a href="index.html">return to overview</a></p>')
+        ofile.write("</div>")
+        ofile.write('<div class="container-fluid"><div class="row">')
+        for i in range(len(self._charts)):
+            ofile.write('<div class="col-sm-{0}">'.format(self._widths[i]))
+            self._charts[i].write_div(ofile)
+            ofile.write("</div>")
+        ofile.write("</div></div>")
+        ofile.write("</body></html>")
+
 
 # parse command line arguments
 argparser = argparse.ArgumentParser()
@@ -154,7 +194,7 @@ args = argparser.parse_args()
 
 # create the output folder
 if os.path.exists(args.output_folder):
-  shutil.rmtree(args.output_folder)
+    shutil.rmtree(args.output_folder)
 os.mkdir(args.output_folder)
 
 # load the time log
@@ -172,25 +212,54 @@ total_time = data["end"].max()
 fields = np.unique(data["label"])
 times = {}
 for field in fields:
-  subdata = data[data["label"] == field]
-  times[field] = {}
-  times[field]["total_time"] = (subdata["end"] - subdata["start"]).sum()
-  times[field]["level"] = subdata["depth"][0]
+    subdata = data[data["label"] == field]
+    times[field] = {}
+    times[field]["total_time"] = (subdata["end"] - subdata["start"]).sum()
+    times[field]["level"] = subdata["depth"][0]
+    times[field]["id"] = subdata["id"][0]
 
 minlevel = data["depth"].min()
 maxlevel = data["depth"].max()
-hfile = HTMLFile()
-hfile.add_chart(TimelineChart(data["start"], data["end"], data["label"],
-                              data["depth"]), True)
+hfile = HTMLFile("Simulation time line")
+hfile.add_chart(
+    TimelineChart(data["start"], data["end"], data["label"], data["depth"]),
+    True,
+)
 for level in range(minlevel, maxlevel + 1):
-  wedges = []
-  labels = []
-  dist = []
-  for field in fields:
-    if times[field]["level"] == level:
-      wedges.append(times[field]["total_time"])
-      labels.append(field)
-      dist.append(0.1)
-  hfile.add_chart(PieChart(wedges, labels, level))
+    wedges = []
+    labels = []
+    ids = []
+    for field in fields:
+        if times[field]["level"] == level:
+            wedges.append(times[field]["total_time"])
+            labels.append(field)
+            ids.append(times[field]["id"])
+    hfile.add_chart(PieChart(wedges, labels, level, ids))
+
+for bid in data["id"]:
+    if (data["pid"] == bid).sum() > 0:
+        subdata = data[data["pid"] == bid]
+        shfile = HTMLFile(data[data["id"] == bid]["label"][0], True)
+        shfile.add_chart(
+            TimelineChart(
+                subdata["start"],
+                subdata["end"],
+                subdata["label"],
+                subdata["depth"],
+            ),
+            True,
+        )
+        shfile.add_chart(
+            PieChart(
+                subdata["end"] - subdata["start"],
+                subdata["label"],
+                0,
+                subdata["id"],
+            )
+        )
+        shfile.write("{0}/block{1}.html".format(args.output_folder, bid))
+    else:
+        shfile = HTMLFile("No subblocks for this block", True)
+        shfile.write("{0}/block{1}.html".format(args.output_folder, bid))
 
 hfile.write("{0}/index.html".format(args.output_folder))
