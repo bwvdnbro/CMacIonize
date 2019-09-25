740a7df28f51e29a566e6d9e4c6dfbba5895190a
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2019-09-25 12:01:57 +0200

    Added Maya's mapping algorithm to PhantomSnapshotDensityFunction.

--- test/testPhantomSnapshotDensityFunction.cpp
+++ test/testPhantomSnapshotDensityFunction.cpp
@@ -37,40 +37,51 @@
  */
 int main(int argc, char **argv) {
 
-  cmac_status("Start reading file...");
-  PhantomSnapshotDensityFunction density_function("Phantomtest.dat", 8000.,
-                                                  nullptr);
-  density_function.initialize();
+  {
+    cmac_status("Start testing old mapping algorithm...");
+    PhantomSnapshotDensityFunction density_function("Phantomtest.dat", 8000.,
+                                                    false, nullptr);
+    density_function.initialize();
 
-  std::ifstream file("Phantom_data.txt");
-  std::string line;
-  uint_fast32_t index = 0;
-  while (getline(file, line)) {
-    std::istringstream lstream(line);
+    std::ifstream file("Phantom_data.txt");
+    std::string line;
+    uint_fast32_t index = 0;
+    while (getline(file, line)) {
+      std::istringstream lstream(line);
 
-    double x, y, z, h;
-    lstream >> x >> y >> z >> h;
+      double x, y, z, h;
+      lstream >> x >> y >> z >> h;
 
-    // convert units
-    x = UnitConverter::to_SI< QUANTITY_LENGTH >(x, "cm");
-    y = UnitConverter::to_SI< QUANTITY_LENGTH >(y, "cm");
-    z = UnitConverter::to_SI< QUANTITY_LENGTH >(z, "cm");
-    h = UnitConverter::to_SI< QUANTITY_LENGTH >(h, "cm");
+      // convert units
+      x = UnitConverter::to_SI< QUANTITY_LENGTH >(x, "cm");
+      y = UnitConverter::to_SI< QUANTITY_LENGTH >(y, "cm");
+      z = UnitConverter::to_SI< QUANTITY_LENGTH >(z, "cm");
+      h = UnitConverter::to_SI< QUANTITY_LENGTH >(h, "cm");
 
-    double tolerance = 1.e-14;
+      double tolerance = 1.e-14;
 
-    CoordinateVector<> p = density_function.get_position(index);
-    assert_values_equal_rel(x, p.x(), tolerance);
-    assert_values_equal_rel(y, p.y(), tolerance);
-    assert_values_equal_rel(z, p.z(), tolerance);
+      CoordinateVector<> p = density_function.get_position(index);
+      assert_values_equal_rel(x, p.x(), tolerance);
+      assert_values_equal_rel(y, p.y(), tolerance);
+      assert_values_equal_rel(z, p.z(), tolerance);
 
-    assert_values_equal_rel(1.e-5, density_function.get_mass(index), tolerance);
-    assert_values_equal_rel(h, density_function.get_smoothing_length(index),
-                            tolerance);
+      assert_values_equal_rel(1.e-5, density_function.get_mass(index),
+                              tolerance);
+      assert_values_equal_rel(h, density_function.get_smoothing_length(index),
+                              tolerance);
 
-    ++index;
+      ++index;
+    }
+    cmac_status("Done testing old mapping algorithm.");
   }
-  cmac_status("Done reading file.");
+
+  {
+    cmac_status("Start testing new mapping algorithm...");
+    PhantomSnapshotDensityFunction density_function("Phantomtest.dat", 8000.,
+                                                    true, nullptr);
+    density_function.initialize();
+    cmac_status("Done testing new mapping algorithm.");
+  }
 
   return 0;
 }
